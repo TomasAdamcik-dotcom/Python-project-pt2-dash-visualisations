04a8b5a3ff144f4e0bdad7a3fc8bf7e2030af1b0
TomasAdamcik-dotcom <adamcik.tomas@gmail.com>
2021-12-10 14:12:59 +0000

    added last three visuals and updated selectors for first visual

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 # dependencies
+from typing import Literal
 import dash
 import dash_bootstrap_components as dbc
 from dash import dcc, html
@@ -6,6 +7,8 @@ from dash.dependencies import Input, Output, State
 import pandas as pd
 from pandas.io.pytables import Selection
 import plotly.express as px
+import csv
+from ast import literal_eval
 
 # initialisations
 app = dash.Dash('',external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -39,31 +42,37 @@ app.layout = html.Div([
             dbc.Row(dbc.Col(html.Div(html.H2('Products performance')))),
             dbc.Row(
                 [
+
                     dbc.Col(html.Div([
                                 html.P('Select product purchases view:'),
                                 dcc.Dropdown(
-                                    id='product-purchases-by-selector', 
+                                    
+                                    id='products-first-dropdown', 
                                     options=[
                                         {'label': 'Overall', 'value': 'overall'},
-                                        {'label': 'By region', 'value': 'by_region'},
-                                        {'label': 'By county', 'value': 'by_county'},
-                                        {'label': 'By product category', 'value': 'by_product_category'}
+                                        {'label': 'By region', 'value': 'region'},
+                                        {'label': 'By county', 'value': 'county'},
+                                        {'label': 'By product category', 'value': 'category'}
                                     ],
                                     placeholder='Select view',
-                                    value='dont-show'
                                 ),
-                                html.P('Select product purchases detail:'),        
-                                dcc.Dropdown(
-                                    id='second_dropdown', 
-                                    options=[
-                                        {'label': 'Overall', 'value': 'overall'}
-                                    ],
-                                    placeholder='Select branch'
-                                    )
+
                     ])),
-                    dbc.Col(html.Div()),
+                    dbc.Col(html.Div([
+                            html.P('Select product purchases detail:'),        
+                            dcc.Dropdown(
+                                id='products-second-dropdown', 
+                                options=[
+                                    {'label': 'none', 'value': 'none'}
+                                ],
+                                placeholder='First, select view',
+                                disabled=True
+                            ),
+                            
+                    ])),
                 ]
             ),
+
             dbc.Row(
                 [
                     dbc.Col(html.Div([       
@@ -84,15 +93,20 @@ app.layout = html.Div([
     ),
         # 2nd visual ############################################
     html.Div([
-            dbc.Row(dbc.Col(html.Div(html.H2('Branches performances')))),
+            dbc.Row(dbc.Col(html.Div(html.H2('Branches performance outlook')))),
             dbc.Row([
                 dbc.Col(
                     dcc.Dropdown(
                         id='branches-performance-dropdown',
+                        # options=[
+                        #     {'label': 'Overall', 'value': 'overall'},
+                        #     {'label': 'Per region', 'value': 'per_region'},
+                        #     {'label': 'Per county', 'value': 'per_county'}
+                        # ],
                         options=[
-                            {'label': 'Overall', 'value': 'overall'},
-                            {'label': 'Per region', 'value': 'per_region'},
-                            {'label': 'Per county', 'value': 'per_county'}
+                            {'label': 'Overall', 'value': 'branch'},
+                            {'label': 'Per region', 'value': 'region'},
+                            {'label': 'Per county', 'value': 'county'}
                         ],
                         placeholder='Select your view'
                     )
@@ -167,158 +181,75 @@ app.layout = html.Div([
 
 # 1st visual ##########################################
 @app.callback(
-    Output(component_id='chart-products-top5',component_property='figure'), # output that goes to chart
-    Output(component_id='chart-products-bottom5',component_property='figure'), # output that goes to chart
-    Input(component_id='product-purchases-by-selector', component_property='value'), # input from 1st dropdown - taking value - used as first function parameter
-    Input(component_id='second_dropdown', component_property='value') ## input from 2ns dropdown - taking value - used as second function parameter
+    Output(component_id='chart-products-top5', component_property='figure'),
+    Output(component_id='chart-products-bottom5', component_property='figure'),
+    Input(component_id='products-first-dropdown', component_property='value')
 )
 
-def update_chart(first_dropdown_selection, second_dropdown_selection):
-    if first_dropdown_selection == 'overall':
+def update_products_drilldown(first_selection):
+    if first_selection == 'overall':
+        # get visual
         visuals_pt1_df_overall = visuals_pt1_df.groupby('product').sum().reset_index().sort_values(by='qty',ascending=False)
-        visuals_pt1_df_overall_top5 = visuals_pt1_df_overall.head(5)
-        visuals_pt1_df_overall_bottom5 = visuals_pt1_df_overall.tail(5)
-        topfigure = px.bar(visuals_pt1_df_overall_top5, x='product', y='qty', title='Top 5 products overall', labels={'product': '', 'qty': 'Quantity of purchased products'})
-        bottomfigure = px.bar(visuals_pt1_df_overall_bottom5, x='product', y='qty', title='Bottom 5 products overall', labels={'product': '', 'qty': 'Quantity of purchased products'})
-        return topfigure, bottomfigure
-    elif first_dropdown_selection == 'by_region' and second_dropdown_selection is not None:
-        # visuals_pt1_df_by_region = visuals_pt1_df.groupby(['region', 'product']).sum().reset_index().sort_values(by='qty',ascending=False)
-        visuals_pt1_df_by_region = visuals_pt1_df[visuals_pt1_df['region'] == 'Wales'].groupby('product').sum().sort_values(by='qty', ascending=False)
-        visuals_pt1_df_overall_top5 = visuals_pt1_df_by_region.head(5)
-        visuals_pt1_df_overall_bottom5 = visuals_pt1_df_by_region.tail(5)
-        topfigure = px.bar(visuals_pt1_df_overall_top5, x='product', y='qty')
-        bottomfigure = px.bar(visuals_pt1_df_overall_bottom5, x='product', y='qty')
-
-        return topfigure, bottomfigure
-    return {}, {}
-
-
+        topfigure_df = visuals_pt1_df_overall.head(5)
+        topfigure_chart = px.bar(topfigure_df, x='product', y='qty', title='Top 5 products overall', labels={'product': '', 'qty': 'Quantity of purchased products'})
+        bottomfigure_df = visuals_pt1_df_overall.tail(5)
+        bottomfigure_chart = px.bar(bottomfigure_df, x='product', y='qty', title='Bottom 5 products overall', labels={'product': '', 'qty': 'Quantity of purchased products'})
+        return topfigure_chart, bottomfigure_chart
+    else:
+        return {}, {}
+    # 1st visual dropdown #############################
 @app.callback(
-    Output(component_id='second_dropdown',component_property='options'),
-    Input(component_id='product-purchases-by-selector', component_property='value')
+    Output(component_id='products-second-dropdown', component_property='disabled'),
+    Output(component_id='products-second-dropdown', component_property='options'),
+    Output(component_id='products-second-dropdown', component_property='placeholder'),
+    Input(component_id='products-first-dropdown', component_property='value'),
 )
 
-def update_second_dropdown(first_dorpdown_value):
-    if first_dorpdown_value == 'by_region':
-        return [{'label': 'East Midlands', 'value': 'East Midlands'},
-                {'label': 'East of England', 'value': 'East of England'}]
+def update_second_drilldown(first_drilldown_selection):
+    if first_drilldown_selection == 'region':
+        with open('data/regions.txt') as f:
+            options = literal_eval(f.read())        
+        return False, options, f'Please select {first_drilldown_selection}'
+    elif first_drilldown_selection == 'county':
+        with open('data/counties.txt') as f:
+            options = literal_eval(f.read())
+        return False, options, f'Please select {first_drilldown_selection}'
+    elif first_drilldown_selection == 'category':
+        with open('data/product_categories.txt') as f:
+            options = literal_eval(f.read())
+        return False, options, f'Please select product {first_drilldown_selection}'
+    else:
+        return True, [], 'First, select view'
 
-    return []
+
 
 # 2nd visual ##########################################
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# East Midlands
-# East of England
-# London
-# North East England
-# North West England
-# Northern Ireland
-# Scotland
-# South East England
-# South West England
-# Wales
-# West Midlands
-# Yorkshire and the Humber
-
-
-"""
 @app.callback(
-    Output(component_id='second-product-drilldown-selector-div', component_property='style'),
-    Output(component_id='chart-products-top5', component_property='figure'),
-    # Output(component_id='chart-products-bottom5', component_property='figure'),
-    Input(component_id='product-purchases-by-selector', component_property='value')
+    Output(component_id='top10-sales-branch', component_property='figure'),
+    Output(component_id='bottom10-sales-branch', component_property='figure'),
+    Output(component_id='top10-qty-branch', component_property='figure'),
+    Output(component_id='bottom10-qty-branch', component_property='figure'),
+    Input(component_id='branches-performance-dropdown' ,component_property='value')
 )
 
-
-def get_summary(visual_1_type, top5):
-    if visual_1_type is not None:
-        if visual_1_type != 'overall':
-            pass
-            # show second dropdown
-            #  return {'display': 'block'}
-
-            if visual_1_type == 'by_region':
-                # pass data to charts
-                
-                pass
-            elif visual_1_type == 'by_county':
-                # pass data to charts
-                
-                pass
-            elif visual_1_type == 'by_product_category':
-                # pass data to charts
-                
-                pass
-        else:     
-            # this is overall case - pass data to visuals - by parameters top5 and bottom5
-            df = visuals_pt1_df.groupby('product').sum().sort_values(by='qty')
-            top5_df = df.head(5)
-            figure = px.bar(top5_df, x='product', y='qty')
-            # bottom5_df = df.tail(5)
-            return top5, figure
+def update_branches_performance(dropdown_selection):
+    if dropdown_selection is not None:
+        sales_df = visuals_pt2_df.groupby(dropdown_selection).sum().reset_index().sort_values(by='amount_in_gbp', ascending=False)
+        top10_sales_df = sales_df.head(10)
+        top10_sales_chart = px.bar(top10_sales_df, x=dropdown_selection, y='amount_in_gbp', title='Top 10 regions by sales')
+        bottom10_sales_df = sales_df.tail(10)
+        bottom10_sales_chart = px.bar(bottom10_sales_df, x=dropdown_selection, y='amount_in_gbp', title='Bottom 10 regions by sales')
+        
+        qty_df = visuals_pt2_df.groupby(dropdown_selection).sum().reset_index().sort_values(by='qty', ascending=False)
+        top10_qty_df = qty_df.head(10)
+        top10_qty_chart = px.bar(top10_qty_df, x=dropdown_selection, y='qty', title='Top 10 regions by sold products')
+        bottom10_qty_df = qty_df.tail(10)
+        bottom10_qty_chart = px.bar(bottom10_qty_df, x=dropdown_selection, y='qty', title='Bottom 10 regions by sold products')
+        
+        return top10_sales_chart, bottom10_sales_chart, top10_qty_chart, bottom10_qty_chart
     else:
-        pass
-
-"""
-
-
-"""
-html.Div([
-        # html.P('Select product purchases view:'),
-        # dcc.Dropdown(
-        #     id='product-purchases-by-selector', 
-        #     options=[
-        #         {'label': 'Overall', 'value': 'overall'},
-        #         {'label': 'By region', 'value': 'by_region'},
-        #         {'label': 'By county', 'value': 'by_county'},
-        #         {'label': 'By product category', 'value': 'by_product_category'}
-        #     ],
-        #     placeholder='Select view',
-        #     value='dont-show'
-        # )
-    ]),
-    html.Div([
-        # html.P('Select product purchases detail:'),        
-        # dcc.Dropdown(
-        #     id='second_dropdown', 
-        #     options=[
-        #         # options should be extract of list
-        #         {'label': 'Overall', 'value': 'overall'}
-        #     ],
-        #     placeholder='Select branch'
-        #     )
-        ], 
-        # id='second-product-drilldown-selector-div', 
-        #     style={'display': 'block'}
-        ),
-
-    html.Span([ 
-        # dcc.Graph(
-        # id='chart-products-top5',
-        # figure={}
-        # ),
-        # dcc.Graph(
-        # id='chart-products-bottom5',
-        # figure={}
-        # )
-    ]),
-    ]),
-
-"""
+        return {}, {}, {}, {}
 
 # run
 app.run_server(debug=True)
